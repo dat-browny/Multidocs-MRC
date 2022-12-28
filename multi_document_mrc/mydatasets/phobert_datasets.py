43f62860462a23c1aafb048e01b6588ca0df7fba
dat-browny <hvtdat.browny@gmail.com>
2022-12-28 16:29:39 +0700

    update datasets v3

--- multi_document_mrc/mydatasets/phobert_datasets.py
+++ multi_document_mrc/mydatasets/phobert_datasets.py
@@ -111,7 +111,7 @@ class ViMRCDatasetsForPhoBERT(ViMRCDatasetsForPhoBERTNoHap):
                     tokenized_examples["has_answer_labels"].append(1)
 
         return tokenized_examples
-
+        
     def prepare_validation_features(self, examples):
         # Some of the questions have lots of whitespace on the left, which is not useful and will make the
         # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
@@ -130,6 +130,58 @@ class ViMRCDatasetsForPhoBERT(ViMRCDatasetsForPhoBERTNoHap):
             return_overflowing_tokens=True,
             return_offsets_mapping=True,
             padding="max_length" if self.data_args.pad_to_max_length else False,
+        )
+
+        # Since one example might give us several features if it has a long context, we need a map from a feature to
+        # its corresponding example. This key gives us just that.
+        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+
+        # For evaluation, we will need to convert our predictions to substrings of the context, so we keep the
+        # corresponding example_id and we will store the offset mappings.
+        tokenized_examples["example_id"] = []
+
+        for i in range(len(tokenized_examples["input_ids"])):
+            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+            sequence_ids = tokenized_examples.sequence_ids(i)
+            context_index = 1 if self.pad_on_right else 0
+
+            # One example can give several spans, this is the index of the example containing this span of text.
+            sample_index = sample_mapping[i]
+            tokenized_examples["example_id"].append(examples["id"][sample_index])
+
+            # Set to None the offset_mapping that are not part of the context so it's easy to determine if a token
+            # position is part of the context or not.
+            tokenized_examples["offset_mapping"][i] = [
+                (o if sequence_ids[k] == context_index else None)
+                for k, o in enumerate(tokenized_examples["offset_mapping"][i])
+            ]
+
+        return tokenized_examples
+
+    def prepare_validation_features_reflection(self, examples):
+        # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+        # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+        # left whitespace
+        question_column_name = "question"
+        context_column_name = "context"
+        max_seq_length = 256
+        pad_to_max_length = True
+        doc_stride = 128
+        pad_on_right = True
+        examples[question_column_name] = [q.lstrip() for q in examples[question_column_name]]
+
+        # Tokenize our examples with truncation and maybe padding, but keep the overflows using a stride. This results
+        # in one example possible giving several features when a context is long, each of those features having a
+        # context that overlaps a bit the context of the previous feature.
+        tokenized_examples = self.tokenizer(
+            examples[question_column_name if pad_on_right else context_column_name],
+            examples[context_column_name if pad_on_right else question_column_name],
+            truncation="only_second" if pad_on_right else "only_first",
+            max_length=max_seq_length,
+            stride=doc_stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length" if pad_to_max_length else False,
         )
 
         # Since one example might give us several features if it has a long context, we need a map from a feature to
@@ -1166,7 +1218,7 @@ class ViMRCReflection(ViMRCDatasetsForPhoBERTNoHap):
 
         x = Dataset.from_dict(dict(examples))
 
-        features = x.map(ViMRCDatasetsForPhoBERT(self.tokenizer).prepare_validation_features)
+        features = x.map(ViMRCDatasetsForPhoBERT(self.tokenizer).prepare_validation_features_reflection)
 
         instance_training = ViMRCDatasetsForPhoBERTNoHapReflection(self.tokenizer).postprocess_qa_predictions(examples=examples, 
                             features=features, 
