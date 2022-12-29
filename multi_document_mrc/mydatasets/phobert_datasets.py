5004d81c902644f0e63d7a495a8adf5e2067473a
dat-browny <hvtdat.browny@gmail.com>
2022-12-29 10:01:30 +0700

    modify map function

--- multi_document_mrc/mydatasets/phobert_datasets.py
+++ multi_document_mrc/mydatasets/phobert_datasets.py
@@ -910,12 +910,16 @@ class ViMRCDatasetsForPhoBERTNoHapReflection(ViMRCDatasetsForPhoBERT):
         for example_index, example in enumerate(tqdm(examples)):
             # Those are the indices of the features associated to the current example.
             feature_indices = features_per_example[example_index]
-
+            if len(feature_indices) ==1:
+                feature_index=feature_indices[0]
+            else:
+                print(scores[feature_indices[0]:feature_indices[-1]])
             feature_index_with_best_score = collections.UserList()
+            print(feature_index_with_best_score)
 
             feature_index_with_best_score.append([index, scores[index]] for index in feature_indices)
 
-            feature_index = sorted(np.array(feature_index_with_best_score).tolist(), key=lambda x: x[1], reverse=True)[0][0]
+            feature_index = sorted(feature_index_with_best_score, key=lambda x: x[1], reverse=True)[0][0]
             prelim_predictions = []
             min_null_prediction = None
             # Looping through all the features associated to the current example.
