64f700e5d62c069852ea05fff8bdc628c4b50b6b
dat-browny <hvtdat.browny@gmail.com>
2022-12-29 11:21:37 +0700

    modify map function

--- multi_document_mrc/mydatasets/phobert_datasets.py
+++ multi_document_mrc/mydatasets/phobert_datasets.py
@@ -993,6 +993,7 @@ class ViMRCDatasetsForPhoBERTNoHapReflection(ViMRCDatasetsForPhoBERT):
                 predictions.append(min_null_prediction)
 
             # Use the offsets to gather the answer text in the original context.
+            print("==========================================================================================================")
             print(example)
             context = example["context"]
             for pred in predictions:
@@ -1234,7 +1235,7 @@ class ViMRCReflection(ViMRCDatasetsForPhoBERTNoHap):
 
 
 
-        print(examples)
+        # print(examples)
 
 
 
