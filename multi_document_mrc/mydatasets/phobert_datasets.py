5f1b4504c3652a629303cb8f02686547b4267dcf
dat-browny <hvtdat.browny@gmail.com>
2023-01-03 09:04:57 +0700

    Update Trainer Reflection

--- multi_document_mrc/mydatasets/phobert_datasets.py
+++ multi_document_mrc/mydatasets/phobert_datasets.py
@@ -1268,30 +1268,131 @@ class ViMRCReflection(ViMRCDatasetsForPhoBERTNoHap):
         return tokenized_examples_
     
     def prepare_validation_features(self, examples):
-        prefix_contexts = [
-            f"{question} {self.tokenizer.sep_token} Tiêu_đề : {title} . Nội_dung :"
-            for question, title in zip(examples[self.question_column_name], examples[self.title_column_name])
-        ]
-        contexts = [
-            f"{prefix_s} {context}"
-            for prefix_s, context in zip(prefix_contexts, examples[self.context_column_name])
-        ]
-
+        # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+        # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+        # left whitespace
+        examples[self.question_column_name] = [q.lstrip() for q in examples[self.question_column_name]]
+        # Tokenize our examples with truncation and maybe padding, but keep the overflows using a stride. This results
+        # in one example possible giving several features when a context is long, each of those features having a
+        # context that overlaps a bit the context of the previous feature.
         tokenized_examples = self.tokenizer(
-            contexts,
-            truncation=True,
+            examples[self.question_column_name if self.pad_on_right else self.context_column_name],
+            examples[self.context_column_name if self.pad_on_right else self.question_column_name],
+            truncation="only_second",
             max_length=self.max_seq_length,
+            stride=self.data_args.doc_stride,
+            return_overflowing_tokens=True,
             return_offsets_mapping=True,
             padding="max_length" if self.data_args.pad_to_max_length else False,
         )
 
-        prefix_ids_lens = [len(self.tokenizer.tokenize(s))+1 for s in prefix_contexts]
-        prefix_mask = [
-            [-10000] * x + [0] * (self.max_seq_length-x)
-            for x in prefix_ids_lens
-        ]
-        tokenized_examples["prefix_mask"] = prefix_mask
-        tokenized_examples["new_context"] = contexts
-        tokenized_examples["example_id"] = examples["id"]
+        # Since one example might give us several features if it has a long context, we need a map from a feature to
+        # its corresponding example. This key gives us just that.
+        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+        # The offset mappings will give us a map from token to character position in the original context. This will
+        # help us compute the start_positions and end_positions.
+        offset_mapping = tokenized_examples["offset_mapping"]
 
-        return tokenized_examples
+        # Let's label those examples!
+        tokenized_examples["start_positions"] = []
+        tokenized_examples["end_positions"] = []
+        tokenized_examples["has_answer_labels"] = []
+
+        for i, offsets in enumerate(offset_mapping):
+            # We will label impossible answers with the index of the CLS token.
+            input_ids = tokenized_examples["input_ids"][i]
+            cls_index = input_ids.index(self.tokenizer.cls_token_id)
+
+            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+            sequence_ids = tokenized_examples.sequence_ids(i)
+
+            # One example can give several spans, this is the index of the example containing this span of text.
+            sample_index = sample_mapping[i]
+            answers = examples[self.answer_column_name][sample_index]
+            # If no answers are given, set the cls_index as answer.
+            if len(answers["answer_start"]) == 0:
+                tokenized_examples["start_positions"].append(cls_index)
+                tokenized_examples["end_positions"].append(cls_index)
+                # has_answer_labels==0 tương ứng với câu hỏi không có câu trả lời, ngược lại
+                tokenized_examples["has_answer_labels"].append(0)
+            else:
+                # Start/end character index of the answer in the text.
+                start_char = answers["answer_start"][0]
+                end_char = start_char + len(answers["text"][0])
+
+                # Start token index of the current span in the text.
+                token_start_index = 0
+                while sequence_ids[token_start_index] != (1 if self.pad_on_right else 0):
+                    token_start_index += 1
+
+                # End token index of the current span in the text.
+                token_end_index = len(input_ids) - 1
+                while sequence_ids[token_end_index] != (1 if self.pad_on_right else 0):
+                    token_end_index -= 1
+
+                # Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
+                if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
+                    tokenized_examples["start_positions"].append(cls_index)
+                    tokenized_examples["end_positions"].append(cls_index)
+                    tokenized_examples["has_answer_labels"].append(0)
+                else:
+                    # Otherwise move the token_start_index and token_end_index to the two ends of the answer.
+                    # Note: we could go after the last offset if the answer is the last word (edge case).
+                    while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
+                        token_start_index += 1
+                    tokenized_examples["start_positions"].append(token_start_index - 1)
+                    while offsets[token_end_index][1] >= end_char:
+                        token_end_index -= 1
+                    tokenized_examples["end_positions"].append(token_end_index + 1)
+                    tokenized_examples["has_answer_labels"].append(1)
+        
+        for k, v in tokenized_examples.items():
+            tokenized_examples[k] = torch.tensor(v, device=self.device)
+        with torch.no_grad(): 
+        #Dungf postprocess cua model MRC de gen instance training cho model nay
+            predictions = self.MRCModel(input_ids=tokenized_examples['input_ids'], 
+                                start_positions=tokenized_examples['start_positions'], 
+                                end_positions=tokenized_examples['end_positions'], 
+                                has_answer_labels=tokenized_examples['has_answer_labels'], 
+                                return_dict=True)
+
+        predictions = (predictions['start_logits'],predictions['end_logits'],predictions['has_answer_probs'],predictions['score'],predictions['head_features'])
+
+        x = Dataset.from_dict(dict(examples))
+        features = x.map(ViMRCDatasetsForPhoBERT(self.tokenizer).prepare_validation_features_reflection,
+                        batched=True,
+                        remove_columns=x.features)
+
+        instance_training = ViMRCDatasetsForPhoBERTNoHapReflection(self.tokenizer, model_name_or_path=self.model_name_or_path).postprocess_qa_predictions(examples=x, 
+                            features=features, 
+                            predictions=predictions,
+                            version_2_with_negative=True,
+                            is_training_reflection=True)
+
+        head_features = [value['head_features'] for key, value in instance_training.items()]
+        feature_index = [value['feature_index'] for key, value in instance_training.items()]
+        tokenized_examples['has_answer_labels'] = tokenized_examples['has_answer_labels'].tolist()
+        tokenized_examples_ = {}
+        tokenized_examples_['input_ids'] = []
+        tokenized_examples_['ans_type_ids'] = []
+        tokenized_examples_['has_answer_labels'] = []
+        tokenized_examples_['attention_mask'] = []
+        tokenized_examples_['head_features'] = []
+        for id, feature_slice in enumerate(feature_index):
+            tokenized_examples_['input_ids'].append(tokenized_examples['input_ids'][feature_slice])
+            tokenized_examples_['has_answer_labels'].append(tokenized_examples['has_answer_labels'][feature_slice])
+            tokenized_examples_['attention_mask'].append(tokenized_examples['attention_mask'][feature_slice])
+            tokenized_examples_['head_features'].append(head_features[id])
+            start_position = tokenized_examples['start_positions'][feature_slice]
+            end_position = tokenized_examples['end_positions'][feature_slice]
+            ans_type_id = torch.tensor([0]*self.max_seq_length)
+            if tokenized_examples_['has_answer_labels'][-1] == 0:
+                ans_type_id[0] = 1
+            else:
+                ans_type_id[0] = 2
+            if start_position < end_position:
+                ans_type_id[start_position] = 3
+                ans_type_id[start_position+1:end_position+1] = 4
+            tokenized_examples_['ans_type_ids'].append(ans_type_id)
+        
+        return tokenized_examples_
---
+++ run_reflection.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright 2020 The HuggingFace Team All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Fine-tuning the library models for question answering using a slightly adapted version of the 🤗 Trainer.
+"""
+
+import logging
+import os
+import sys
+import datasets
+import evaluate
+import transformers
+from multi_document_mrc.trainer import ReflectionTrainer
+from transformers import (
+    Trainer,
+    DataCollatorWithPadding,
+    EvalPrediction,
+    HfArgumentParser,
+    PreTrainedTokenizerFast,
+    TrainingArguments,
+    default_data_collator,
+    set_seed,
+)
+from transformers.trainer_utils import get_last_checkpoint
+from transformers.utils.versions import require_version
+from multi_document_mrc.arguments import ModelArguments, DataTrainingArguments
+from multi_document_mrc.models_map import get_model_version_classes
+
+
+require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/question-answering/requirements.txt")
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    # See all possible arguments in src/transformers/training_args.py
+    # or by passing the --help flag to this script.
+    # We now keep distinct sets of args, for a cleaner separation of concerns.
+
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    model_architecture = get_model_version_classes(model_args.model_architecture)
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    log_level = training_args.get_process_log_level()
+    logger.setLevel(log_level)
+    datasets.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.enable_default_handler()
+    transformers.utils.logging.enable_explicit_format()
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+    )
+    logger.info(f"Training/evaluation parameters {training_args}")
+
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+
+    # Set seed before initializing model.
+    set_seed(training_args.seed)
+
+    # Load pretrained model and tokenizer
+    #
+    # Distributed training:
+    # The .from_pretrained methods guarantee that only one local process can concurrently
+    # download model & vocab.
+    config = model_architecture.config_class.from_pretrained(
+        model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+        revision=model_args.model_revision,
+        use_auth_token=True if model_args.use_auth_token else None,
+    )
+    # add properties to config
+    config.model_architecture = model_args.model_architecture
+
+    tokenizer = model_architecture.tokenizer_class.from_pretrained(
+        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+        use_fast=True,
+        revision=model_args.model_revision,
+        use_auth_token=True if model_args.use_auth_token else None,
+    )
+    model = model_architecture.model_class.from_pretrained(
+        model_args.model_name_or_path,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=config,
+        cache_dir=model_args.cache_dir,
+        revision=model_args.model_revision,
+        use_auth_token=True if model_args.use_auth_token else None,
+    )
+
+    # Tokenizer check: this script requires a fast tokenizer.
+    if not isinstance(tokenizer, PreTrainedTokenizerFast):
+        raise ValueError(
+            "This example script only works for models that have a fast tokenizer. Checkout the big table of models at"
+            " https://huggingface.co/transformers/index.html#supported-frameworks to find the model types that meet"
+            " this requirement"
+        )
+
+    # Datasets
+    dataset_obj = model_architecture.dataset_class(
+        tokenizer=tokenizer,
+        data_args=data_args,
+        cache_dir=model_args.cache_dir,
+        do_train=training_args.do_train,
+        do_eval=training_args.do_eval,
+        do_predict=training_args.do_predict,
+        model_name_or_path=model_args.model_name_or_path
+    )
+
+    train_dataset = dataset_obj.get_train_dataset(
+        main_process_first=training_args.main_process_first
+    )
+    eval_dataset, eval_examples = dataset_obj.get_eval_dataset(
+        main_process_first=training_args.main_process_first
+    )
+    predict_dataset, predict_examples = dataset_obj.get_predict_dataset(
+        main_process_first=training_args.main_process_first
+    )
+
+    # Data collator 
+    # We have already padded to max length if the corresponding flag is True, otherwise we need to pad in the data
+    # collator.
+    data_collator = (
+        default_data_collator
+        if data_args.pad_to_max_length
+        else DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None)
+    )
+
+    metric = evaluate.load("f1")
+
+    def compute_metrics(p: EvalPrediction):
+        print(p.predictions)
+        print(p.label_ids)
+        return metric.compute(predictions=p.predictions, references=p.label_ids)
+
+    # Initialize our Trainer
+    trainer = ReflectionTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval else None,
+        eval_examples=eval_examples if training_args.do_eval else None,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        post_process_function=dataset_obj.post_processing_function,
+        compute_metrics=compute_metrics,
+    )
+
+    # Training
+    if training_args.do_train:
+        checkpoint = None
+        if training_args.resume_from_checkpoint is not None:
+            checkpoint = training_args.resume_from_checkpoint
+        elif last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+
+        metrics = train_result.metrics
+        max_train_samples = (
+            data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
+        )
+        metrics["train_samples"] = min(max_train_samples, len(train_dataset))
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+    # Evaluation
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+        metrics = trainer.evaluate()
+
+        max_eval_samples = data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
+        metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+    # Prediction
+    if training_args.do_predict:
+        logger.info("*** Predict ***")
+        results = trainer.predict(predict_dataset, predict_examples)
+        metrics = results.metrics
+
+        max_predict_samples = (
+            data_args.max_predict_samples if data_args.max_predict_samples is not None else len(predict_dataset)
+        )
+        metrics["predict_samples"] = min(max_predict_samples, len(predict_dataset))
+
+        trainer.log_metrics("predict", metrics)
+        trainer.save_metrics("predict", metrics)
+
+    kwargs = {"finetuned_from": model_args.model_name_or_path, "tasks": "question-answering"}
+    if data_args.dataset_name is not None:
+        kwargs["dataset_tags"] = data_args.dataset_name
+        if data_args.dataset_config_name is not None:
+            kwargs["dataset_args"] = data_args.dataset_config_name
+            kwargs["dataset"] = f"{data_args.dataset_name} {data_args.dataset_config_name}"
+        else:
+            kwargs["dataset"] = data_args.dataset_name
+
+
+if __name__ == "__main__":
+    main()
