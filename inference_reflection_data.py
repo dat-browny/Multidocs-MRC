a1c9a371d9ac8ebb90f1e1e52a1572a7ab38a883
dat-browny <hvtdat.browny@gmail.com>
2023-01-12 08:00:06 +0700

    modify ViReflection

--- inference_reflection_data.py
+++ inference_reflection_data.py
@@ -112,7 +112,7 @@ def convert_to_instance(model, tokenizer, examples, tokenized_data, device, batc
         # if start_position < end_position:
         #     ans_type_id[start_position] = 3
         #     ans_type_id[start_position+1:end_position+1] = 4
-        # tokenized_examples_['ans_type_ids'].append(ans_type_id)
+        tokenized_examples_['ans_type_ids'].append(ans_type_id)
 
     return datasets.Dataset.from_dict(tokenized_examples_)
 
