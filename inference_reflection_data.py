fcec4810f5ea6d6f2b7b4ceb30adec1bd9e95547
dat-browny <hvtdat.browny@gmail.com>
2023-01-12 09:34:50 +0700

    modify ViReflection

--- inference_reflection_data.py
+++ inference_reflection_data.py
@@ -244,12 +244,8 @@ def main():
 
     model_.to(device)
     
-
-    print(eval_examples)
-    print(eval_dataset)
-
-    # train_dataset = convert_to_instance(model=model, tokenizer=tokenizer, examples=train_examples, tokenized_data=train_dataset, device=device, batch_size=32, model_name_or_path=model_args.model_name_or_path, max_seq_length=data_args.max_seq_length)
-    eval_dataset = convert_to_instance(model=model, tokenizer=tokenizer, examples=eval_examples, tokenized_data=eval_dataset, device=device, batch_size=32, model_name_or_path=model_args.model_name_or_path, max_seq_length=data_args.max_seq_length)
+    train_dataset = convert_to_instance(model=model_, tokenizer=tokenizer, examples=train_examples, tokenized_data=train_dataset, device=device, batch_size=32, model_name_or_path=model_args.model_name_or_path, max_seq_length=data_args.max_seq_length)
+    eval_dataset = convert_to_instance(model=model_, tokenizer=tokenizer, examples=eval_examples, tokenized_data=eval_dataset, device=device, batch_size=32, model_name_or_path=model_args.model_name_or_path, max_seq_length=data_args.max_seq_length)
     data_collator = (
         default_data_collator
         if data_args.pad_to_max_length
