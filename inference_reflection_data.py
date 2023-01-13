a624a50303b85c6edc5e1b3a4a7f3915434cd2e1
dat-browny <hvtdat.browny@gmail.com>
2023-01-13 10:49:38 +0700

    modify ViReflection

--- inference_reflection_data.py
+++ inference_reflection_data.py
@@ -67,7 +67,7 @@ def convert_to_instance(model, tokenizer, examples, tokenized_data, device, batc
                                     has_answer_labels=batch['has_answer_labels'].to(device), 
                                     return_dict=True)
             while i<3:
-                print(output['head_features'])
+                print(torch.isnan(output['head_features'].any()))
                 i+=1
     
             start_logits += output['start_logits'].tolist()
