02f1dff4a59553e10da0c4203a0fd0287ca280f8
dathvt4424 <dat1912bg@gmail.com>
2023-01-28 11:10:59 +0700

    Debug

--- run_reflection.py
+++ run_reflection.py
@@ -182,7 +182,10 @@ def main():
     metric = evaluate.load("f1")
 
     def compute_metrics(p: EvalPrediction):
-        return classification_report(p.label_ids, p.predictions)
+        print(p.label_ids)
+        print(p.predictions)
+        target = ['class 0', "class 1"]
+        return classification_report(p.label_ids, p.predictions, labels=[0,1])
         # return metric.compute(predictions=p.predictions, references=p.label_ids)
 
 
