fbb00420af8e666c6c98cbf663f3ca19e9a468cb
dat-browny <hvtdat.browny@gmail.com>
2023-02-02 08:43:26 +0700

    Debug MRC

--- inference_reflection_data.py
+++ inference_reflection_data.py
@@ -234,7 +234,7 @@ def main():
     model_.to(device)
     
     trainer = QuestionAnsweringTrainer(
-        model=model,
+        model=model_,
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
