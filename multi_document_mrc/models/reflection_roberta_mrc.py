27a500f8866e3b153594f75bb2740202b103a6cd
dathvt4424 <dat1912bg@gmail.com>
2023-02-13 09:44:00 +0700

    Add hidden layer

--- multi_document_mrc/models/reflection_roberta_mrc.py
+++ multi_document_mrc/models/reflection_roberta_mrc.py
@@ -473,7 +473,7 @@ class RobertaForMRCClassification(RobertaPreTrainedModel):
             return_dict=return_dict,
             output_hidden_states=self.config.output_hidden_states,
         )
-        print(outputs.shape)
+
         if self.config.output_hidden_states:
             hidden_states = outputs.hidden_states
             x = torch.cat(tuple([hidden_states[i] for i in range(-self.num_hidden_states, 0, -1)]), dim=-1)
